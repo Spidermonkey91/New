c6779ebe27767726b09a7ac77ad18b322c256d9b
Devin Wells <Djay_wells@yahho.com>
2021-08-23 12:35:22 -0500

    First update

---
+++ Index.js
@@ -0,0 +1,14 @@
+//Devin Wells
+
+function btnDropClick(){
+    console.log("btnDrop executed!")
+    $('.content-drop').toggleClass("hide");
+}
+
+$('.email').click(function(e) {  
+    document.getElementById('email').style.backgroundColor="rgba(0, 0, 0, 0.009)";
+    document.getElementById('email').style.borderColor="grey";
+
+});
+
+//a
